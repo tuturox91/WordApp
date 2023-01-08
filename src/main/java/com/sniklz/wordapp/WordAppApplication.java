267e0608cf0b883ee0a189b20d8cce789c9606b7
Igor Bukur <tuturox91@gmail.com>
2023-01-08 11:56:13 +0200

    Initial commit

---
+++ src/main/java/com/sniklz/wordapp/WordAppApplication.java
@@ -0,0 +1,13 @@
+package com.sniklz.wordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordAppApplication.class, args);
+    }
+
+}
